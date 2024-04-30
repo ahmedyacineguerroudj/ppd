5e3a3c71e3302dbbf8a09241a94243c0b33e4094
khadidjasg <khadidjasg09@gmail.com>
2024-04-30 15:27:48 +0100

    Ajout de la page connexion

--- src/Page connexion et inscription/login.js
+++ src/Page connexion et inscription/login.js
@@ -1,21 +1,45 @@
-const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
+const loginRadio = document.querySelector("#login");
+const signupRadio = document.querySelector("#signup");
+const sliderTab = document.querySelector(".slider-tab");
 
-signupBtn.onclick = (()=>{
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
+let isLoginActive = true;
+
+function updateView() {
+    if (isLoginActive) {
+        loginForm.style.marginLeft = "0%";
+        signupForm.style.marginLeft = "0%";
+        loginRadio.checked = true;
+        signupRadio.checked = false;
+        sliderTab.style.left = "0%";
+    } else {
+        loginForm.style.marginLeft = "-50%";
+        signupForm.style.marginLeft = "0%";
+        loginRadio.checked = false;
+        signupRadio.checked = true;
+        sliderTab.style.left = "50%";
+    }
+}
+
+updateView();
+
+loginBtn.addEventListener("click", () => {
+    isLoginActive = true;
+    updateView();
 });
 
-loginBtn.onclick = (()=>{
-    loginForm.style.marginLeft = "0%";
-    loginText.style.marginLeft = "0%";
+signupBtn.addEventListener("click", () => {
+    isLoginActive = false;
+    updateView();
 });
 
-signupLink.onclick = (()=>{
-    signupBtn.click();
-    return false;
-});
 
+signupLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    isLoginActive = false;
+    updateView();
+});
