b9d4fdce045c9c6b3a7a9ae2bad2caf74ab3eed0
khadidjasg <khadidjasg09@gmail.com>
2024-05-18 11:08:58 +0100

    Ajout de la page login/signup final

--- src/Page connexion et inscription/login.js
+++ src/Page connexion et inscription/login.js
@@ -25,6 +25,7 @@ function updateView() {
     }
 }
 
+
 updateView();
 
 loginBtn.addEventListener("click", () => {
@@ -43,3 +44,53 @@ signupLink.addEventListener("click", (e) => {
     isLoginActive = false;
     updateView();
 });
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const signupForm = document.querySelector('form.signup');
+    const verificationModal = document.getElementById('verificationModal');
+    const closeModal = document.querySelector('.modal .close');
+    const verifyCodeButton = document.getElementById('verifyCodeButton');
+    let formSubmitted = false; // Variable pour suivre si le formulaire a déjà été soumis
+
+    function showModal() {
+        verificationModal.style.display = 'block';
+    }
+
+    function hideModal() {
+        verificationModal.style.display = 'none';
+    }
+
+    closeModal.addEventListener('click', hideModal);
+    window.addEventListener('click', function (event) {
+        if (event.target === verificationModal) {
+            hideModal();
+        }
+    });
+
+    signupForm.addEventListener('submit', function (event) {
+        if (!formSubmitted) {
+            event.preventDefault();
+            console.log('Verification code sent to email.');
+            showModal();
+        }
+    });
+
+    verifyCodeButton.addEventListener('click', function () {
+        const verificationCode = document.getElementById('verificationCode').value;
+        if (verificationCode === '123456') {
+            hideModal();
+            formSubmitted = true;
+            signupForm.submit();
+        } else {
+            alert('Invalid verification code. Please try again.');
+        }
+    });
+});
+
